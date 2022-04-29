df79f7714e02c01b5715e06f48d3b651aa0ea763
Alexis Guzman <alexis12989x@gmail.com>
2022-04-29 16:43:53 -0500

    test(jest): create test for get a list of all students certified

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,18 +1,25 @@
 const StudentService = require('./../../lib/services/StudentService');
 
 describe('Unit test for StudentService class', () => {
+  const student1 = {
+    id: '6264d5d89f1df827eb84bb23',
+    name: 'Warren',
+    email: 'Todd@visualpartnership.xyz',
+    credits: 508,
+    enrollments: ['Visual Thinking Intermedio', 'Visual Thinking Avanzado'],
+    previousCourses: 1,
+    haveCertification: true,
+  };
+
   test('Get a list of all students', () => {
-    const student1 = {
-      id: '6264d5d89f1df827eb84bb23',
-      name: 'Warren',
-      email: 'Todd@visualpartnership.xyz',
-      credits: 508,
-      enrollments: ['Visual Thinking Intermedio', 'Visual Thinking Avanzado'],
-      previousCourses: 1,
-      haveCertification: true,
-    };
     const students = StudentService.getAllStudents();
 
     expect(students[0]).toStrictEqual(student1);
+  });
+
+  test('Get a list of students that have certification', () => {
+    const certifiedStudents = StudentService.getAllCertificate();
+
+    expect(certifiedStudents[0]).toStrictEqual(student1);
   });
 });
